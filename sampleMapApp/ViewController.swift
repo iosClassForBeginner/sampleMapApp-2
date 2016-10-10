8bf3ebe5cad518fb2c584fb7d9eaf762f0d9b002
WataruMaeda <wm19850716@gmail.com>
2016-10-10 17:08:36 +0900

    Map実装

--- sampleMapApp/ViewController.swift
+++ sampleMapApp/ViewController.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import MapKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var myMap: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // 緯度・軽度を設定
+        let location = CLLocationCoordinate2DMake(34.404,135.308)
+        myMap.setCenterCoordinate(location,animated:true)
+        
+        // 縮尺を設定
+        var region = myMap.region
+        region.center = location
+        region.span.latitudeDelta = 0.02
+        region.span.longitudeDelta = 0.02
+        myMap.setRegion(region,animated:true)
+        
+        // 表示タイプを設定
+        myMap.mapType = MKMapType.Standard
+//        myMap.mapType = MKMapType.Satellite
+//        myMap.mapType = MKMapType.Hybrid
+        
+        // ピンの設定
+        let pin = MKPointAnnotation()
+        pin.coordinate = CLLocationCoordinate2DMake(34.404,135.308)
+        pin.title = "iOS Development Meetup!"
+        pin.subtitle = "10/15はColab難波にて、地図アプリを作ります"
+        myMap.addAnnotation(pin)
     }
 
     override func didReceiveMemoryWarning() {
